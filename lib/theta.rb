7efc9a3942b1b15e8d9ccb3fcfb3df4fe467ea9c
Chris <ctoneal@gmail.com>
2011-06-22 08:26:56 -0500

    REPL enhancements

--- lib/theta.rb
+++ lib/theta.rb
@@ -25,32 +25,53 @@ module Theta
 		# start an interactive interpreter
 		def repl
 			puts "For interpreter commands, type 'help'"
+			indent_count = 0
+			code = []
 			while true
-				print "theta> "
+				print "theta> " + ("  " * indent_count)
 				input = gets.chomp
-				case input
-				#when "clear"
-				#	puts "Resetting environment..."
-				#	@interpreter = Interpreter.new
-				when "exit", 24.chr
-					puts "Exiting..."
-					return
-				when "help"
-					repl_help
+				code << input
+				temp_code = code.join(" ")
+				open_parens = temp_code.count("(")
+				close_parens = temp_code.count(")")
+				if open_parens == close_parens
+					case input
+					when "clear"
+						puts "Resetting environment..."
+						@interpreter = Interpreter.new
+					when "exit", 24.chr
+						puts "Exiting..."
+						return
+					when "help"
+						repl_help
+					else
+						begin
+							output = @interpreter.run(code.join(" "))
+							if not output.nil? and not output.empty? 
+								output.each { |value| puts @interpreter.make_readable(value) }
+							end
+						rescue SyntaxError
+						rescue RuntimeError => e
+							puts "Undefined variable: #{e.message}"
+						rescue NoMethodError, TypeError => e
+							puts "Invalid operation: #{e.message}"
+						ensure
+							code = []
+							indent_count = 0						
+						end
+					end
+				elsif open_parens > close_parens
+					indent_count = open_parens - close_parens
 				else
-					begin
-						output = @interpreter.run(input)
-					rescue SyntaxError
-					end
-					if not output.nil? and not output.empty? 
-						output.each { |value| puts @interpreter.make_readable(value) }
-					end
+					puts "Parentheses don't match.  Ignoring code."
+					code = []
+					indent_count = 0
 				end
 			end
 		end
 		
 		def repl_help
-			#puts "'clear' resets the environment"
+			puts "'clear' resets the environment"
 			puts "'exit' or Ctrl-X will exit the interpreter"
 			puts "'help' will display this message"
 		end
